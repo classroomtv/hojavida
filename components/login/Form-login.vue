0dd859eac3e580a417100737a40e17823b2df116
Joseph Jauregui <joseph.jauregui@classroomtv.com>
2022-12-06 18:09:35 -0300

    configuracion inicial del entorno y desarrollo de pagina de prueba

---
+++ components/login/Form-login.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+       <div class="row">
+        <div class="col-10 offset-1 mt-5 pt-5">
+            <img src="img/logowhite.png" class="img-fluid" alt="">
+            <p class="text-white float-end">Hoja de vida</p>
+        </div>
+        <div class="col-10 offset-1 mt-2">
+            <input type="text" class="form-control form-control-lg" placeholder="Username">
+        </div>
+        <div class="col-10 offset-1 mt-4">
+            <input type="text" class="form-control form-control-lg" placeholder="Password">
+        </div>
+        <div class="col-10 offset-1 mt-5 pt-5 d-grid">
+           <button class="btn btn-secondary btn-lg">Log in</button>
+        </div>
+       </div>
+    </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,16 @@ export default {
   nitro: {
     preset: 'vercel-edge',
   },
+  static:{
+    prefix: false
+  },
+  css:[
+    '@/css/master.css',
+    "bootstrap/dist/css/bootstrap.min.css"
+  ],
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
+    },
+  },
 };
